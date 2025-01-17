5f9f407f5e3ae89334777086fa91a575545c421e
Ankita Das <daszankita23@gmail.com>
2025-01-17 23:06:45 +0530

    revise

--- Day-6/07_StackandHeap.js
+++ Day-6/07_StackandHeap.js
@@ -1,32 +1,43 @@
-let MyName="Ankita";
-let Hername=MyName;
- Hername="hello";
- console.log(MyName);
- console.log(Hername);
+// let MyName="Ankita";
+// let Hername=MyName;
+//  Hername="hello";
+//  console.log(MyName);
+//  console.log(Hername);
 
 
- let userOne ={
-    email:"Ankiita@gamil.com",
-    id: "123dgf"
- }
+//  let userOne ={
+//     email:"Ankiita@gamil.com",
+//     id: "123dgf"
+//  }
 
- let userTwo=userOne;
+//  let userTwo=userOne;
 
- userTwo.email ="ankitadyst.com"
- userTwo.id = "gfhf"
- console.log(userOne);
- console.log(userTwo);
-
-
+//  userTwo.email ="ankitadyst.com"
+//  userTwo.id = "gfhf"
+//  console.log(userOne);
+//  console.log(userTwo);
 
 
 
- let Myacc = {
-      email:"ankita",
-      roll:"10"
- }
+
+
+//  let Myacc = {
+//       email:"ankita",
+//       roll:"10"
+//  }
  
- let Mybank=Myacc;
- Mybank.email="bkash";
- console.log(Myacc);
- console.log(Mybank);
+//  let Mybank=Myacc;
+//  Mybank.email="bkash";
+//  console.log(Myacc);
+//  console.log(Mybank);
+// let userone = {
+//    name:"Ankita",
+//    roll:23,
+//    upi:123,
+// }
+// let usertwo = userone
+// usertwo.roll=50
+// console.log(usertwo.roll);
+// console.log(userone.roll);
+
+
--- Day-6/08_String.js
+++ Day-6/08_String.js
@@ -1,35 +1,43 @@
- const name="Annkita";
- const Repocount=27;
- console.log(name+Repocount);
- console.log(`Hello my name is ${name} and my repo count is${Repocount}`);
- const gamename=new String("ankitadas__ankita__ank")
- console.log(gamename);
- console.log(gamename.__proto__);
- console.log(gamename.toUpperCase());
- console.log(gamename.charAt(5));
- console.log(gamename.indexOf("t"));
- const newString= gamename.substring(-8,4)//dividing into substring
-console.log(newString);//printing the characters from 0th indeex to 3rd index 4th will not be included
+//  const name="Annkita";
+//  const Repocount=27;
+//  console.log(name+Repocount);
+//  console.log(`Hello my name is ${name} and my repo count is${Repocount}`);
+//  const gamename=new String("ankitadas__ankita__ank")
+//  console.log(gamename);
+//  console.log(gamename.__proto__);
+//  console.log(gamename.toUpperCase());
+//  console.log(gamename.charAt(5));
+//  console.log(gamename.indexOf("t"));
+//  const newString= gamename.substring(-8,4)//dividing into substring
+// console.log(newString);//printing the characters from 0th indeex to 3rd index 4th will not be included
  
 
 
-const anotherString=gamename.slice(1,7)
-console.log(anotherString);
+// const anotherString=gamename.slice(1,7)
+// console.log(anotherString);
 
-const newString1="    ankita    ";
-console.log(newString1.trim());
-
-
-
-const url="https://ankita.com/ankita%20das";
-console.log(url.replace('%20','__'));
+// const newString1="    ankita    ";
+// console.log(newString1.trim());
 
 
-console.log(url.includes("monkey"));
+
+// const url="https://ankita.com/ankita%20das";
+// console.log(url.replace('%20','__'));
 
 
-console.log(gamename.split("__"));
+// console.log(url.includes("monkey"));
 
-const myname="smrutirekha";
-const slicednegative=myname.slice(-11,-1)
-console.log(slicednegative)
+
+// // console.log(gamename.split("__"));
+
+// // const myname="smrutirekha";
+// // const slicednegative=myname.slice(-11,-1)
+// // console.log(slicednegative)
+const name="Ankita"
+const repo=27
+console.log(`Hello my name is ${name} and repo count is ${repo}`);
+const gmename = new String('Ankita')
+console.log(gmename[0]);
+console.log(gmename.__proto__);
+console.log(gmename.length) ;
+
