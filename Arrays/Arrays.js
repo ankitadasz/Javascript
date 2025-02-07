c7b3a22222f27eb65c10d3c3915323530209464d
Ankita Das <daszankita23@gmail.com>
2025-02-07 20:28:37 +0530

    basics

---
+++ Arrays/Arrays.js
@@ -0,0 +1,14 @@
+const myArr = [0,1,2,3,4,5]
+const myFood =["pizza","puchka","pasta"]
+
+// console.log(myFood[1]);
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(45)
+// myArr.shift()
+// console.log(myArr.includes(3));
+// console.log(myArr.indexOf(3));
+const newArr= myArr.join()
+console.log(myArr)
+console.log(typeof newArr);
