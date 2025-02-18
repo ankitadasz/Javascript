3dc4f2a212fc65c996999e7c9c5710ab522b1e42
Ankita Das <daszankita23@gmail.com>
2025-02-18 21:14:43 +0530

    js

---
+++ piyushgarg/conditional.js
@@ -0,0 +1,32 @@
+// //LADDER IFELSE
+// const age=2;
+// // if(age>=18){
+// //     console.log("You can vote🤗");
+// // }
+// // else{
+// //     console.log("You can not vote🤗");
+    
+// // }
+// //tERNARY OPERATOR
+// let result=age>=18 ? console.log("yes"):"no";
+// console.log(result);
+
+
+
+const option = 1;
+switch(option){
+    case 1: {
+        console.log('hello')
+        break;
+    }
+    case 2: {
+        console.log('hello world')
+         break;
+    }
+    case 3: {
+        console.log();
+        break;
+    }
+    default: console.log("IDK");
+    
+}
---
+++ piyushgarg/loop.js
@@ -0,0 +1,19 @@
+// let ip=0;
+// let house=100;
+// while(ip!=house){
+//     ip=ip+1;
+//     console.log("step taken"+ip);
+    
+// }
+
+let number=40
+let guess=0
+
+do{
+    guess=parseInt(prompt("guess a number😃"))
+    if(guess==number){
+        alert('winner🧀❤️👌')
+        break;
+    }
+}
+while(guess!=9)
