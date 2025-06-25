3d74bc35c8c17512f0b088310e77ffcb7c66f7dc
Ankita Das <daszankita23@gmail.com>
2025-06-25 11:51:42 +0530

    Weather app

--- piyushgarg/Weather-App/script.js
+++ piyushgarg/Weather-App/script.js
@@ -3,6 +3,7 @@ const input=document.getElementById("city");
 const cityName=document.getElementById("city-name");
 const cityTime=document.getElementById("city-time");
 const cityTemp=document.getElementById("city-temp");
+const cityHumi=document.getElementById("city-humi");
 
 async function getData(cityName){
     const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=24556431ad994a6380585907252406&q=${cityName}&aqi=yes`)
@@ -14,8 +15,9 @@ return await promise.json()
 button.addEventListener("click",async ()=>{
     const value = input.value;
     const result = await getData(value);
-    cityName.innerText=`${result.location.name},${result.location.region}-${result.location.country}`
-    cityTime.innerText=result.location.localtime;
-    cityTemp.innerText=result.current.temp_c;
+    cityName.innerHTML=`<i class="fa-solid fa-cloud"></i>Weather in:${result.location.name},${result.location.region}-${result.location.country}`
+    cityTime.innerHTML=`<i class="fa-solid fa-earth-americas"></i>LocalTime:${result.location.localtime}`;
+    cityTemp.innerHTML=`<i class="fa-solid fa-temperature-three-quarters"></i>Temprature:${result.current.temp_c}°C`;
+    cityHumi.innerHTML=`<i class="fa-solid fa-tint"></i>Humidity:${result.current.humidity}%`;
 });
 
