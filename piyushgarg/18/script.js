0b7e0e5f53225c43056ea455bb65fa114342d41f
Ankita Das <daszankita23@gmail.com>
2025-07-02 14:21:45 +0530

    Currying in js

--- piyushgarg/18/script.js
+++ piyushgarg/18/script.js
@@ -1,5 +1,26 @@
-function  add(a,b,c){
-    return a+b+c;
-}
+// function add(a) {
+//   return function (b) {
+//     return function (c) {
+//       return a + b + c;
+//     };
+//   };
+// }
+// console.log(add(2)(3)(5)); //this will not exicute untill you have given all the parameters
+const add = (a) => (b) => (c) => a + b + c;
+console.log(add(2)(3)(5));
 
-console.log(add(2,3,5));
+// function sendAutoEmail(to) {
+//   return function (subject) {
+//     return function (body) {
+//       console.log(`Sending Email to ${to} with subject ${subject}:${body}`);
+//     };
+//   };
+// }
+
+const sendAutoEmail = (to) => (subject) => (body) =>
+  `Sending Email to ${to} with subject ${subject}:${body}`;
+let step1 = sendAutoEmail("daszankita23@gmail.com");
+let step2 = step1("New Order Confirmation");
+let step3 = step2("hey ankita you order will be delivered tommorow");
+console.log(step3);
+
