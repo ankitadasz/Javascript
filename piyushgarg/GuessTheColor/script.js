3492fb41da4dcbfcf0a96e575d3b0dc1bac87f19
Ankita Das <daszankita23@gmail.com>
2025-07-05 00:04:06 +0530

    Guess The Color

--- piyushgarg/GuessTheColor/script.js
+++ piyushgarg/GuessTheColor/script.js
@@ -1,6 +1,7 @@
 const colorcode=document.getElementById("color-code");
 const option=document.getElementById("option");
 let randomcolor=null;
+let score=0;
 function generateRandomNumber(min, max) {
      return min + Math.floor(Math.random() * (max - min + 1));
 }
@@ -9,12 +10,20 @@ function genrateRandomColorRGB() {
     const red=generateRandomNumber(0,255);
     const green=generateRandomNumber(0,255);
     const blue=generateRandomNumber(0,255);
-     return `rgb(${red},${green},${blue})`;
+     return `rgb(${red}, ${green}, ${blue})`;
 
+}
+function incrementStore(){
+    score+=1;
+    document.getElementById("score").innerText=score;
 }
  function validateResult(el){
    console.log(el.target);
    const selectedColor=el.target.style.backgroundColor;
+   if((randomcolor===selectedColor)){
+    incrementStore();
+   }
+   
  }
 function startGame(){
     randomcolor=genrateRandomColorRGB();
