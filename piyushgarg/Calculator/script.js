4e7452b4d4adb31c7156bb75e9ab56b75621798a
Ankita Das <daszankita23@gmail.com>
2025-07-06 00:27:35 +0530

    Calculator

--- piyushgarg/Calculator/script.js
+++ piyushgarg/Calculator/script.js
@@ -1,5 +1,15 @@
+const input=document.getElementById("num-input");
+const Answer=document.getElementById("Ans");
+
 
 function handleClickButton(el){
     console.log(el.innerText);
-    
+    const exp=`${input.value}${el.innerText}`;
+    input.value=exp;
+    Answer.value=eval(exp);
+}
+
+function reset(){
+    input.value="";
+    Answer.value="";
 }
